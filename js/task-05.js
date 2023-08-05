3392ea6745874df315e61208ca82afe158e7b1c6
Elvira-Karp <125654804+Elvira-Karp@users.noreply.github.com>
2023-08-05 13:17:04 +0300

    дз6

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+const textInput = document.querySelector('#name-input');
+const output = document.querySelector('#name-output');
+console.log(output)
+
+textInput.addEventListener('input', event => {
+    output.textContent = event.target.value
+
+    if (event.target.value === '  ') {
+        output.textContent = 'Anonymous'
+    }
+})
